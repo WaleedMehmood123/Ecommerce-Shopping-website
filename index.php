7669e62ab2ee2c6e3cd182d1f68c4c7746317fc9
WaleedMehmood123 <waleedmehmood054@gmail.com>
2023-08-19 17:49:36 +0500

    Displaying static cart design & fetching cart items

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ include('functions/common_function.php');
           <a class="nav-link" href="#">Register</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup><?php cart_item(); ?></sup></a>
+          <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"></i><sup><?php cart_item(); ?></sup></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Total Price: <?php total_cart_price(); ?>/-</a>
