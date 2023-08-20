292f31c26e8e5a2ecce66592d08bb8279b741db3
WaleedMehmood123 <waleedmehmood054@gmail.com>
2023-08-20 11:53:18 +0500

    Updating Quantities & Removing items from cart

--- cart.php
+++ cart.php
@@ -78,6 +78,7 @@ cart();
 <!-- fourth child table -->
 <div class="container">
     <div class="row">
+      <form action="" method="post">
         <table class="table table-bordered text-center">
             <thead>
                 <tr>
@@ -92,7 +93,6 @@ cart();
             <tbody>
               <!-- php code to display dynamic data -->
               <?php
-              global $conn;
               $get_ip_address = getIPAddress();
               $total_price=0;
               $cart_query = "Select * from cart_details where ip_address='$get_ip_address'";
@@ -113,12 +113,23 @@ cart();
                 <tr>
                     <td><?php echo $product_title?></td>
                     <td><img src="admin_area/product_images/<?php echo $product_image1?>" alt="apple" class="cart_img"></td>
-                    <td><input type="text" class="form-input w-50"></td>
+                    <td><input type="text" name="qty" class="form-input w-50"></td>
+                    <?php
+                    $get_ip_address = getIPAddress();
+                    if(isset($_POST['update_cart'])){
+                      $quantities = $_POST['qty'];
+                      $update_cart = "update cart_details set quantity=$quantities where ip_address='$get_ip_address'";
+                      $result_products_quantity =mysqli_query($conn,$update_cart);
+                      $total_price=$total_price*$quantities;
+                    }
+                    ?>
                     <td><?php echo $price_table?>/-</td>
-                    <td><input type="checkbox"></td>
+                    <td><input type="checkbox" name="removeitem[]" value="<?php echo $product_id?>"></td>
                     <td>
-                        <button class="bg-info px-3 py-2 border-0 mx-3">Update</button>
-                        <button class="bg-info px-3 py-2 border-0 mx-3">Remove</button>
+                        <!-- <button class="bg-info px-3 py-2 border-0 mx-3">Update</button> -->
+                        <!-- <button class="bg-info px-3 py-2 border-0 mx-3">Remove</button> -->
+                        <input type="submit" value="Update Cart" name="update_cart" class="bg-info px-3 py-2 border-0 mx-3">
+                        <input type="submit" value="Remove Cart" name="remove_cart" class="bg-info px-3 py-2 border-0 mx-3">
                     </td>
                 </tr>
                 <?php
@@ -133,6 +144,25 @@ cart();
             <a href="index.php"><button class="bg-info px-3 py-2 border-0 mx-3">Continue Shopping</button></a>
             <a href="#"><button class="bg-secondary px-3 py-2 border-0 text-light">Checkout</button></a>
         </div>
+        </form>
+        <!-- function to remove items -->
+        <?php
+        function remove_cart_item(){
+          global $conn;
+          if(isset($_POST['remove_cart'])){
+            foreach($_POST['removeitem'] as $remove_id){
+              echo $remove_id;
+              $delete_query = "Delete from cart_details where product_id = $remove_id";
+              $run_delete = mysqli_query($conn,$delete_query);
+              if($run_delete){
+                echo "<script>window.open('cart.php','_self')</script>";
+              }
+            }
+          }
+        }
+        echo $remove_item = remove_cart_item();
+
+        ?>
     </div>
 </div>
 
@@ -141,6 +171,6 @@ cart();
 include('includes/footer.php');
 ?>
 <!-- Bootstrap JS Link -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></>
 </body>
 </html>
